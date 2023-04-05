23d9a62407a705548aff4bd42867f94965398d1d
joydas <joy600508@gmail.com>
2023-04-06 00:27:46 +0600

    change

---
+++ src/components/Social.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './social.css';
+
+const SocialBar = () => {
+  return (
+    <div className="social-bar">
+      <div className="spot"></div>
+      <div className="spot"></div>
+      <div className="spot"></div>
+      <div className="spot"></div>
+      <div className="spot"></div>
+      <div className="spot"></div>
+      <div className="spot"></div>
+      <div className="social-icon"><a href="https://github.com/Anirbandasjoy"><i className="fab fa-github"></i></a></div>
+      <div className="social-icon"><a href="https://www.linkedin.com/in/anirbandasjoy/?fbclid=IwAR0yvh5UcpUtA4lJEaGfRYEWQ2dl3FqVi7f9jCN84W60u_d1VXdBIkbkpXU"><i className="fab fa-linkedin"></i></a></div>
+      <div className="spot"></div>
+      <div className="spot"></div>
+      <div className="spot"></div>
+      <div className="spot"></div>
+      <div className="spot"></div>
+      <div className="spot"></div>
+      <div className="spot"></div>
+    </div>
+  );
+};
+
+export default SocialBar;
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -5,12 +5,14 @@ import HeroImag from '../components/HeroImag'
 import Navbar from '../components/Navbar'
 import CardSkillSection from '../components/Skills'
 import Work from '../components/Work'
+import SocialBar from '../components/Social'
 
 const Contac = () => {
   return (
     <div>
         <Navbar />
         <HeroImag />
+        <SocialBar />
         <CardSkillSection />
         <Work />
         <Footer />
